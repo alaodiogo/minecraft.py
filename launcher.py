06075d24ed755d453cb55c6fb14b9d93799f1520
Diogo Oliveira <89609323+alaodiogo@users.noreply.github.com>
2026-05-19 22:20:26 -0300

    Update launcher.py to include configuration and subprocess
    Refactor Minecraft launcher to use configurations and subprocess for launching.

--- launcher.py
+++ launcher.py
@@ -1,8 +1,17 @@
+import subprocess
 from minecraft_launcher_lib import *
-diretorio_mine = "/home/alaodiogo/Documentos/Minecraft/"
-conta = microsoft_account.get_auth_code_from_url(url: str)
 
-#def minecraft_launcher():
+diretorio_mine = "/home/alaodiogo/Documentos/Minecraft/"
+versao = 0
+configuracoes = {
+    "username": "alaodiogo",
+    "uuid": "841e42a3-86b7-4f7f-ae5d-5959bd1a17c3",
+    "token": "token"
+    }
+
 versao = input("Digite a versão do Minecraft que deseja instalar: ")
 install.install_minecraft_version(versao, diretorio_mine)
-command.get_minecraft_command(versao, diretorio_mine, conta)
+print("Instalação concluída!")
+
+iniciar_mine = command.get_minecraft_command(versao, diretorio_mine, configuracoes)
+subprocess.run(iniciar_mine)
